b8ebf93471d70a5d83928c6ad87aca6cfe801110
Eugenia Kim <elk303@nyu.edu>
2019-07-25 21:05:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,7 +49,7 @@
     return newarray
   end 
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point=0)
   i = 0 
   
   while i < source_array.length do 
